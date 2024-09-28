76d605652a8932ce42b1464378e92b5b92d0df6d
marcapedoh <adralabel@gmail.com>
2024-09-28 17:42:28 +0200

    initialize project

---
+++ src/app/store/actions/movie.action.ts
@@ -0,0 +1,7 @@
+import { createAction, props } from "@ngrx/store";
+import { Movie } from "src/app/models/Movie";
+
+
+export const getMovies= createAction('[Movie] get movies');
+export const addMovies= createAction('[Movie] get movies', props<{movie:Movie}>)
+export const addMoviesWithoutProps= createAction('[Movie] get movies',(movie:Movie)=> movie )
